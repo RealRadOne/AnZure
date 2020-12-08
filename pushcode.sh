c04b0ae34d063800a04ec7381b524d9bc58acf20
RealRadOne <shrishtisakshi311@gmail.com>
2020-12-09 05:02:28 +0530

    Adding updates

--- pushcode.sh
+++ pushcode.sh
@@ -4,4 +4,4 @@ git config --global user.name "RealRadOne"
 git add .
 git pull origin main
 git commit -m "Adding updates"
-git push "https://RealRadOne:SAKSHIsinh7@98@github.com/AnZure/repo.git master"
+git push -u origin master
