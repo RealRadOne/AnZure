d0b85f6a54e03ea066952aa0f01be8b563a90730
RealRadOne <shrishtisakshi311@gmail.com>
2020-12-09 05:11:45 +0530

    Adding updates

--- pushcode.sh
+++ pushcode.sh
@@ -2,6 +2,7 @@ echo "Hello!"
 git config credential.helper store
 git config --global user.email "shrishtisakshi311@gmail.com"
 git config --global user.name "RealRadOne"
+git config --global user.password "SAKSHIsinh7@98"
 git add .
 git pull origin main
 git commit -m "Adding updates"
