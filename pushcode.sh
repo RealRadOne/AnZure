161ca87b95d72571a22732a6a9c4764e8be26309
RealRadOne <shrishtisakshi311@gmail.com>
2020-12-08 15:35:17 +0530

    Adding updates

---
+++ pushcode.sh
@@ -0,0 +1,7 @@
+echo "Hello!"
+git add .
+git pull origin main
+git config --global user.email "shrishtisakshi311@gmail.com"
+git config --global user.name "RealRadOne"
+git commit -m "Adding updates"
+git push origin main
