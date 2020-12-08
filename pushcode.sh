29091e09125e7a8fea16fe462e65226281fca571
RealRadOne <shrishtisakshi311@gmail.com>
2020-12-09 05:08:25 +0530

    Adding updates

--- pushcode.sh
+++ pushcode.sh
@@ -1,5 +1,5 @@
 echo "Hello!"
-git comfig --global credential.helper wincred
+git config --global credential.helper wincred
 git config --global user.email "shrishtisakshi311@gmail.com"
 git config --global user.name "RealRadOne"
 git add .
